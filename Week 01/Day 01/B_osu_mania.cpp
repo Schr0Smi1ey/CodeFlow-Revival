16af3597fa6cec148a125f2523314edb58891e33
Schr0Sm1ley <radiantremel444@gmail.com>
2024-10-17 12:03:33 +0600

    Day 01

---
+++ Week 01/Day 01/B_osu_mania.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define int long long int
+#define trace(x) cout << #x << " : " << x << endl;
+const int MOD = 1e9 + 7;
+const int INF = 1e18, MINF = -1e18;
+const int N = 2e5 + 5;
+
+void solution() {
+    int n;cin >> n;
+    vector<string> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    for(int i = n - 1; i >= 0; i--) {
+        cout << v[i].find('#') + 1 << " ";
+    }
+    cout << endl;
+}
+
+int32_t main() {
+    fastio;
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solution();
+    }
+    return 0;
+}
