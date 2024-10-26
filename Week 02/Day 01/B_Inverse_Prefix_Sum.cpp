f8332a5a18e5f68309c3bb02f49de0249f69f693
Schr0Sm1ley <radiantremel444@gmail.com>
2024-10-27 01:19:09 +0600

    Week 02 Day 01

---
+++ Week 02/Day 01/B_Inverse_Prefix_Sum.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define int long long int
+#define trace(x) cout << #x << " : " << x << endl;
+const int MOD = 1e9 + 7;
+const int INF = 1e18, MINF = -1e18;
+const int N = 2e5 + 5;
+
+void solution() {
+    int n;cin >> n;
+    int arr[n];
+    for (int i = 0;i < n; i++) {
+        cin >> arr[i];
+        if(i) {
+            cout << arr[i] - arr[i - 1] << " ";
+            continue;
+        }
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+int32_t main() {
+    fastio;
+    int t = 1;
+    while(t--) {
+        solution();
+    }
+    return 0;
+}
